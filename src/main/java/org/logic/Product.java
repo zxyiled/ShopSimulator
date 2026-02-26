027365611043d26ffbad8f706a2b19e223bed261
zxyiled <sebastianmejiadev@gmail.com>
2026-02-25 21:47:32 -0500

    Updating Product.java and Validator.java

--- src/main/java/org/logic/Product.java
+++ src/main/java/org/logic/Product.java
@@ -1,4 +1,45 @@
 package org.logic;
 
 public class Product {
+
+    private String code;
+    private String name;
+    private double price;
+    private int quantity;
+
+    //Constructor
+    public Product(String code, String name, double price, int quantity) {
+        this.code = code;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    //Getters and Setters
+    public String getCode() {
+        return code;
+    }
+    public void setCode(String code) {
+        this.code = code;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getQuantity(){
+        return quantity;
+    }
+    public void setQuantity(int quantity){
+        this.quantity = quantity;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Code: %s | Name: %s | Quantity: %d | Price: $%.2f", code, name, quantity, price);
+    }
 }
--- src/main/java/org/logic/Validator.java
+++ src/main/java/org/logic/Validator.java
@@ -1,4 +1,43 @@
 package org.logic;
+import java.util.List;
+import java.util.Optional;
 
 public class Validator {
+
+    private static final int minimum_stock_alert = 5;
+
+    public static boolean validateCode(String code) {
+     return code != null && !code.trim().isEmpty() && code.matches("[A-Za-z0-9]+");
+    }
+
+    public static boolean validateName(String name) {
+        return name != null && !name.trim().isEmpty() && name.length() >= 3;
+    }
+
+    public static boolean validateQuantity(int quantity) {
+        return quantity >= 0;
+    }
+
+    public static boolean validatePrice(double price) {
+        return price > 0;
+    }
+
+    public static boolean validateProductNonExistent(List<Product> products, String code) {
+        return products.stream().noneMatch(p -> p.getCode().equals(code));
+    }
+
+    public static boolean validateEnoughStock(Product product, int requiredQuantity) {
+        return product.getQuantity() >= requiredQuantity;
+    }
+
+    public static boolean validateQuantityOperation(int quantity) {
+        return quantity > 0;
+    }
+
+    public static boolean isStockLow(Product product) {
+        return product.getQuantity() <= minimum_stock_alert;
+    }
+
+    //Get custom error message
+
 }
