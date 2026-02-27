5520affc991d95e13e945ff22cee67104df31898
zxyiled <sebastianmejiadev@gmail.com>
2026-02-27 17:21:15 -0500

    Creating a private constructor that prevents instantiation of the class. Ain't public because we are only using static methods

--- src/main/java/org/logic/Validator.java
+++ src/main/java/org/logic/Validator.java
@@ -3,7 +3,11 @@ import java.util.List;
 
 public class Validator {
 
-    private static final int minimum_stock_alert = 5;
+    private Validator() {
+        // Utility class - prevent instantiation
+    }
+
+    private static final int MINIMUM_STOCK_ALERT = 5;
 
     public static boolean validateCode(String code) {
      return code != null && !code.trim().isEmpty() && code.matches("[A-Za-z0-9]+");
@@ -25,6 +29,10 @@ public class Validator {
         return products.stream().noneMatch(p -> p.getCode().equals(code));
     }
 
+    public static boolean validateProductExistent(List<Product> products, String code) {
+        return products.stream().anyMatch(p -> p.getCode().equals(code));
+    }
+
     public static boolean validateEnoughStock(Product product, int requiredQuantity) {
         return product.getQuantity() >= requiredQuantity;
     }
@@ -34,14 +42,14 @@ public class Validator {
     }
 
     public static boolean isStockLow(Product product) {
-        return product.getQuantity() <= minimum_stock_alert;
+        return product.getQuantity() <= MINIMUM_STOCK_ALERT;
     }
 
     //Get custom error message
     public static String getMsgError(String typeError) {
         return switch (typeError) {
             case "code" -> "Error: Invalid Code. Has to contain only letters and numbers";
-            case "nname" -> "Error: Invalid Name. Minimum length is 3 characters";
+            case "name" -> "Error: Invalid Name. Minimum length is 3 characters";
             case "quantity" -> "Error: Invalid Quantity. Has to be greater than or equal to 0";
             case "price" -> "Error: Invalid Price. Has to be greater than 0";
             case "existent" -> "Error: The product already exists";
