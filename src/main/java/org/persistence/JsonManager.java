043808ff200b7599aa17c33170fa55c55ce7ffe5
zxyiled <sebastianmejiadev@gmail.com>
2026-02-27 17:08:55 -0500

    Adding the persistence package and JsonManager.java class to start with the persistence later

---
+++ src/main/java/org/persistence/JsonManager.java
@@ -0,0 +1,11 @@
+package org.persistence;
+
+import java.io.File;
+
+public class JsonManager {
+
+    private static final String DATA_DIRECTORY = "data";
+    private static final String PRODUCTS_FILE = "products.json" + File.separator + products.json;
+    private static final String ALERTS_FILE = "alerts.json" + File.separator + alerts.json;
+
+}
