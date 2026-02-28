10753a47c71546638c5cc49b53bbfb6a14ceeb83
zxyiled <sebastianmejiadev@gmail.com>
2026-02-27 23:16:23 -0500

    Adding new constant variables to stop using String literals in message outputs. Also, deleting some redundancies between validateProductNonExistent and validateProductExistent methods

--- src/main/java/org/logic/Validator.java
+++ src/main/java/org/logic/Validator.java
@@ -1,5 +1,6 @@
 package org.logic;
 import java.util.List;
+import java.util.Optional;
 
 public class Validator {
 
@@ -7,7 +8,20 @@ public class Validator {
         // Utility class - prevent instantiation
     }
 
-    private static final int MINIMUM_STOCK_ALERT = 5;
+    public static final int MINIMUM_STOCK_ALERT = 5;
+
+    // --- Public static constants for not use literal strings ---
+
+    public static final String ERROR_EXISTENT = "existent";
+    public static final String ERROR_CODE = "code";
+    public static final String ERROR_NAME = "name";
+    public static final String ERROR_QUANTITY = "quantity";
+    public static final String ERROR_PRICE = "price";
+    public static final String ERROR_NON_EXISTENT = "non_existent";
+    public static final String ERROR_INSUFFICIENT_STOCK = "insufficient_stock";
+    public static final String ERROR_OPERATION_QUANTITY = "operation_quantity";
+
+    // --- Public static methods ---
 
     public static boolean validateCode(String code) {
      return code != null && !code.trim().isEmpty() && code.matches("[A-Za-z0-9]+");
@@ -26,11 +40,11 @@ public class Validator {
     }
 
     public static boolean validateProductNonExistent(List<Product> products, String code) {
-        return products.stream().noneMatch(p -> p.getCode().equals(code));
+        return products.stream().noneMatch(p -> p.getCode().equalsIgnoreCase(code));
     }
 
-    public static boolean validateProductExistent(List<Product> products, String code) {
-        return products.stream().anyMatch(p -> p.getCode().equals(code));
+    public static boolean validateProductExistent(Optional<Product> product) {
+        return product.isPresent();
     }
 
     public static boolean validateEnoughStock(Product product, int requiredQuantity) {
@@ -48,14 +62,14 @@ public class Validator {
     //Get custom error message
     public static String getMsgError(String typeError) {
         return switch (typeError) {
-            case "code" -> "Error: Invalid Code. Has to contain only letters and numbers";
-            case "name" -> "Error: Invalid Name. Minimum length is 3 characters";
-            case "quantity" -> "Error: Invalid Quantity. Has to be greater than or equal to 0";
-            case "price" -> "Error: Invalid Price. Has to be greater than 0";
-            case "existent" -> "Error: The product already exists";
-            case "no_existent" -> "Error: The product does not exist or was not found";
-            case "insufficient_Stock" -> "Error: The product does not have enough stock";
-            case "operation_quantity" -> "Error: The quantity has to be greater than 0";
+            case ERROR_CODE -> "Error: Invalid Code. Has to contain only letters and numbers";
+            case ERROR_NAME -> "Error: Invalid Name. Minimum length is 3 characters";
+            case ERROR_QUANTITY -> "Error: Invalid Quantity. Has to be greater than or equal to 0";
+            case ERROR_PRICE -> "Error: Invalid Price. Has to be greater than 0";
+            case ERROR_EXISTENT -> "Error: The product already exists";
+            case ERROR_NON_EXISTENT -> "Error: The product does not exist or was not found";
+            case ERROR_INSUFFICIENT_STOCK -> "Error: The product does not have enough stock";
+            case ERROR_OPERATION_QUANTITY -> "Error: The quantity has to be greater than 0";
             default -> "Error: Failed to validate";
         };
     }
