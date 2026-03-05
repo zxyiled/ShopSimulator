0d1893bdda3ab17e07aeabe781ed90a9adcea5d4
zxyiled <sebastianmejiadev@gmail.com>
2026-03-04 23:16:54 -0500

    Deleting setCode method from Product.java

--- src/main/java/org/logic/Product.java
+++ src/main/java/org/logic/Product.java
@@ -27,9 +27,6 @@ public class Product {
     public String getCode() {
         return code;
     }
-    public void setCode(String code) {
-        this.code = code;
-    }
 
     public String getName() {
         return name;
