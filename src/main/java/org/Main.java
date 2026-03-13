ae28049f7ff9f2cae6e554efdb2f66c0ac4ed587
zxyiled <sebastianmejiadev@gmail.com>
2026-03-13 17:42:45 -0500

    Update to work properly with Spring Boot

--- src/main/java/org/Main.java
+++ src/main/java/org/Main.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.logging.Logger;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"org.controller", "org.config"})
 public class Main {
 
     public static final Logger logger = Logger.getLogger(Main.class.getName());
