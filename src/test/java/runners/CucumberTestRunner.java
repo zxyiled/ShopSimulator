85d847b5c9125fd6cc12b37bdfb2fef727f53a1c
zxyiled <sebastianmejiadev@gmail.com>
2026-04-11 11:19:32 -0500

    Feat: new feats on build.gradle, azure-pipelines.yml & cucumbtestrunner. build.gradle had some changes, deleting unused plugins and redundant dependences. also, implementing comments to separate each dependencies and setting the getenv to tryna run SonarQ in Azure Azure-pipelines has uncommented the Step 6 or the SonarQ analysis to run it on Azure CucumberTestRunner had a new implementation with annotations, using @Suite, @IncludeEngines and @SelectClasspathResource - Suite: establishes the test container in the class marked (CucumberTestRunner) - IncludeEngines: specifies the test engines to include in the suite. In this case, Cucumber because we need to run Cucumber scenarios - SelectClasspathResource: tells the test engine where to find test resources (features)

--- src/test/java/runners/CucumberTestRunner.java
+++ src/test/java/runners/CucumberTestRunner.java
@@ -1,8 +1,12 @@
 package runners;
 
-import io.cucumber.junit.platform.engine.Cucumber;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-@Cucumber
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
 public class CucumberTestRunner {
 
 }
